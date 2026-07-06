0c23a74e6af1087b1d56026ecefdfa5845db8162
anhkhoa23git <buianhkhoa978@gmail.com>
2026-07-06 17:15:37 +0700

    Fix: Correct import format for SafeHavenFrame and SafeHavenMoneyLadder

--- components/millionaire/GameplayScreen.tsx
+++ components/millionaire/GameplayScreen.tsx
@@ -3,7 +3,8 @@
 import { useState, useEffect, useCallback, useRef } from "react";
 import { AnswerBox } from "./AnswerBox";
 import { LifelinesBar } from "./LifelinesBar";
-import { SafeHavenFrame } from "./SafeHavenFrame";`nimport { SafeHavenMoneyLadder } from "./SafeHavenMoneyLadder";
+import { SafeHavenFrame } from "./SafeHavenFrame";
+import { SafeHavenMoneyLadder } from "./SafeHavenMoneyLadder";
 import { CountdownTimer } from "./CountdownTimer";
 import { Question, formatMoney } from "@/lib/millionaire/questions";
 import { PrizeStep } from "@/lib/millionaire/prize";
